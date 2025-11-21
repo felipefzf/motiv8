cb3e02b8b4a65f5b0de08f5f4d6b4e11d68aca08
Felipe Zamorano <felipe.zamorano.f@gmail.com>
2025-11-21 03:26:07 -0300

    ahora si
    arreglao

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const API_URL = import.meta.env.PROD 
-  ? 'https://motiv8-api.onrender.com' // <-- Tu URL real de Render
+  ? 'https://motiv8-luwi.onrender.com' // <-- Tu URL real de Render
   : 'http://localhost:5000';
 export default API_URL;
